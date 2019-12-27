c80120354bbebb89450737e111006d9d7dbaafc9
clay.stup@gmail.com <cstupi@Claytons-Air.lan>
2019-12-26 16:58:40 -0800

    vote ws ready

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ import { ApolloClient } from 'apollo-client'
 import { createHttpLink } from 'apollo-link-http'
 import { InMemoryCache } from 'apollo-cache-inmemory'
 import { split } from 'apollo-link'
-import { WebSocketLink } from 'apollo-ws-link'
+import { WebSocketLink } from 'apollo-link-ws'
 import { getMainDefinition } from 'apollo-utilities'
 
 
